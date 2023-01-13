97ff5ac5938e4e9d95e8f204a97adbf637cea6e6
sunghun choi <csh6222sp@gmail.com>
2023-01-13 19:05:36 +0900

    add react-input-mask

--- src/components/CustomDateInput.tsx
+++ src/components/CustomDateInput.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import InputMask, {ReactInputMask} from "react-input-mask";
 
 function CustomDateInput() {
+    const fromDateInputRef = useRef<HTMLInputElement>(null);
+    const toDateInputDivRef = useRef<HTMLDivElement>(null);
+
+    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.nativeEvent.isComposing || e.key !== 'Enter') return
+        console.log("Enter!")
+        if(toDateInputDivRef) {
+            toDateInputDivRef.current?.querySelector("input")?.focus();
+        }
+    }
+
     return (
         <div>
-            <span>Date : </span>
+            <div>
+                <span>From Date : </span>
+                <InputMask onKeyDown={handleKeyDown} mask="9999-99-99" />
+            </div>
+            <div ref={toDateInputDivRef}>
+                <span>To Date : </span>
+                <InputMask mask="9999-99-99" />
+            </div>
         </div>
     )
 
