06e8b6b67cf557b8ec479221e9de41fa836d84e2
sunghun choi <csh6222sp@gmail.com>
2023-01-13 19:44:39 +0900

    add mui input fields

--- src/components/CustomDateInput.tsx
+++ src/components/CustomDateInput.tsx
@@ -1,5 +1,9 @@
 import React, {useRef} from 'react';
 import InputMask from "react-input-mask";
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 function CustomDateInput() {
     const toDateInputDivRef = useRef<HTMLDivElement>(null);
@@ -13,14 +17,31 @@ function CustomDateInput() {
 
     return (
         <div>
-            <div>
-                <span>From Date : </span>
-                <InputMask onKeyDown={handleFromDateInputEnterKeyDown} mask="9999-99-99" />
-            </div>
-            <div ref={toDateInputDivRef}>
-                <span>To Date : </span>
-                <InputMask mask="9999-99-99" />
-            </div>
+            <Paper
+                component="form"
+                sx={{ display: 'flex', alignItems: 'center', width: "450px" }}
+            >
+                <div>
+                    <InputMask onKeyDown={handleFromDateInputEnterKeyDown} mask="9999-99-99">
+                        <InputBase
+                            sx={{ ml: 1, flex: 1, minWidth: "140px" }}
+                            placeholder="FromDate"
+                            inputProps={{ 'aria-label': 'from date' }}
+                        />
+                    </InputMask>
+                </div>
+                <ArrowRightAltIcon  sx={{ p: '10px' }} />
+                <div ref={toDateInputDivRef}>
+                    <InputMask mask="9999-99-99">
+                        <InputBase
+                            sx={{ ml: 1, flex: 1, minWidth: "140px" }}
+                            placeholder="ToDate"
+                            inputProps={{ 'aria-label': 'to date' }}
+                        />
+                    </InputMask>
+                </div>
+                <CalendarMonthIcon sx={{ p: '10px' }} />
+            </Paper>
         </div>
     )
 
