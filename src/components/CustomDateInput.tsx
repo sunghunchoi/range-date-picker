3ac975cf635082ab7064387b27d81e2fa441e60e
sunghun choi <csh6222sp@gmail.com>
2023-01-19 18:26:38 +0900

    fix calendar

--- src/components/CustomDateInput.tsx
+++ src/components/CustomDateInput.tsx
@@ -7,31 +7,79 @@ import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import {ActiveModifiers, ClassNames, DateFormatter, DateRange, DayPicker} from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
 import ja from 'date-fns/locale/ja';
-import {addDays, format, isAfter, isBefore, isEqual} from "date-fns";
+import {addDays, format, isAfter, isBefore, isEqual, subDays} from "date-fns";
 
 interface CustomDateInputState {
     dateRange : DateRange;
-    dateRangeString: {from: string, to: string};
+    dateRangeString: {from: string, fromError: boolean, to: string, toError: boolean};
     showCalendar: boolean;
     inputFocus: string | null;
 }
 
+type dateRangeSelectedTarget = "FROM" | "TO"
+
 const today = new Date();
 const firstDayOnMonth = new Date(today.getFullYear(), today.getMonth(), 1);
 const maxEndDay = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 1);
+
+const getDiffDays = (startDate: Date, endDate: Date): number => {
+    return (startDate.getTime() - endDate.getTime()) / (1000*60*60*24);
+}
+
+
 const getMaxEndDay = (startDate: Date):Date => {
-    const diffFromMaxEndDays = (maxEndDay.getTime() - startDate.getTime()) / (1000*60*60*24);
+    const diffFromMaxEndDays = getDiffDays(maxEndDay, startDate);
     if(diffFromMaxEndDays < 60) {
         return maxEndDay;
     }
     return addDays(startDate, 59);
 }
 
+
 const isOverMaxEndDay = (targetDate: Date): boolean => {
     return isAfter(targetDate.setHours(0,0,0), maxEndDay);
 }
 
+const adjustFromToDate = (
+    selectedDate: Date,
+    prevFromTo: {from: Date, to: Date},
+    selectedTarget: dateRangeSelectedTarget): { from: Date, to: Date } | null =>
+{    const overMaxEndDay = isOverMaxEndDay(selectedDate);
+    if(overMaxEndDay) return null;
+    if (selectedTarget === "FROM") {
+        if(isAfter(selectedDate, prevFromTo.to)) {
+            return {from: selectedDate, to: getMaxEndDay(selectedDate)}
+        }
+        const diff = getDiffDays(prevFromTo.to, selectedDate);
+        return diff > 59 ?
+            {from: selectedDate, to: addDays(selectedDate, 59)}
+            :
+            {from: selectedDate, to: prevFromTo.to}
+    } else if(selectedTarget === "TO") {
+        if(isBefore(selectedDate, prevFromTo.from)) {
+            return {from: subDays(selectedDate, 59), to: selectedDate}
+        }
+        const diff = getDiffDays(selectedDate, prevFromTo.from);
+        return diff > 59 ?
+            {from: subDays(selectedDate, 59), to: selectedDate}
+            :
+            {from:prevFromTo.from, to:selectedDate }
+    }
+    return prevFromTo;
+}
 
+const css = `
+  .custom-selected:not([disabled]) {
+    font-size: 1rem;  
+    background-color: #32a88f;
+    border: 2px solid #32a88f;
+}
+  .custom-selected:hover:not([disabled]) { 
+    border-color: red;
+    border: 2px solid green;
+    color: green;
+  }
+`;
 const formatCaption: DateFormatter = (date, options) => {
     return (
         <>
@@ -49,7 +97,9 @@ function CustomDateInput() {
             },
             dateRangeString: {
                 from: format(firstDayOnMonth, 'yyyyMMdd'),
+                fromError: false,
                 to: format(maxEndDay, 'yyyyMMdd'),
+                toError: false,
             },
             showCalendar: false,
             inputFocus: null,
@@ -61,7 +111,7 @@ function CustomDateInput() {
 
     function handleEscKeyDown(e: any) {
         if (e.key !== 'Escape') return
-        setState({...state, showCalendar: false});
+        setState((prev) => ({...prev, showCalendar: false, inputFocus: null}));
     }
 
     React.useEffect(() => {
@@ -73,6 +123,9 @@ function CustomDateInput() {
 
     const handleFromDateInputEnterKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.nativeEvent.isComposing || e.key !== 'Enter') return
+        if( state.dateRangeString.fromError ) {
+            return;
+        }
         if(toDateInputDivRef) {
             toDateInputDivRef.current?.querySelector("input")?.focus();
             setState((prev) => ({...prev, inputFocus: "to"}))
@@ -81,6 +134,9 @@ function CustomDateInput() {
 
     const handleFromToInputEnterKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.nativeEvent.isComposing || e.key !== 'Enter') return;
+        if( state.dateRangeString.toError ) {
+            return;
+        }
         setState((prev) => ({...prev, showCalendar: false, inputFocus: null}));
     }
 
@@ -96,92 +152,115 @@ function CustomDateInput() {
         activeModifiers: ActiveModifiers, e: React.MouseEvent
     ) => {
         e.preventDefault();
-        if(state.inputFocus === "to" && isEqual(selectedDay , state.dateRange.to ?? new Date())) {
-            setState((prev) => ({...prev, showCalendar: false, inputFocus: null}));
+        if(state.inputFocus === "to" && isEqual(selectedDay , state.dateRange.to!)) {
+            setState((prev) => ({...prev, inputFocus: null}));
         }
         if(range && range.from && range.to) {
-            if(state.inputFocus === "to" && isBefore(selectedDay.setHours(0,0,0), state.dateRange.from?.setHours(0,0,0) ?? new Date())) {
-                return;
+            if(state.inputFocus === "from" || state.inputFocus === null) {
+                const targetRange = adjustFromToDate(
+                    selectedDay,
+                    {from: state.dateRange.from!, to: state.dateRange.to!},
+                    "FROM"
+                );
+                if(targetRange) {
+                    setState((prev) => (
+                        {
+                            ...prev,
+                            dateRangeString: {
+                                ...prev.dateRangeString,
+                                from: format(targetRange.from, 'yyyyMMdd'),
+                                fromError: false,
+                                to: format(targetRange.to, 'yyyyMMdd'),
+                            },
+                            dateRange: {
+                                from: targetRange.from, to: targetRange.to
+                            },
+                            inputFocus: "to",
+                        }))
+                    return;
+                }
             }
-            if(state.inputFocus === "from") {
-                 setState((prev) => (
-                     {
-                        ...prev,
-                        dateRangeString: {
-                            from: format(selectedDay, 'yyyyMMdd'),
-                            to: format(
-                                isAfter(state.dateRange.to ?? selectedDay, getMaxEndDay(selectedDay))
-                                    ? getMaxEndDay(selectedDay) : state.dateRange.to ?? selectedDay,'yyyyMMdd')
-                        },
-                        dateRange: {from: selectedDay, to: isAfter(state.dateRange.to ?? selectedDay, getMaxEndDay(selectedDay)) ? getMaxEndDay(selectedDay) : state.dateRange.to},
-                        inputFocus: "to",
-                     }))
-                 return;
+            if(state.inputFocus === "to") {
+                const targetRange = adjustFromToDate(
+                    selectedDay,
+                    {from: state.dateRange.from!, to: state.dateRange.to!},
+                    "TO"
+                );
+                if(targetRange) {
+                    setState((prev) => (
+                        {
+                            ...prev,
+                            dateRangeString: {
+                                ...prev.dateRangeString,
+                                from: format(targetRange.from, 'yyyyMMdd'),
+                                toError: false,
+                                to: format(targetRange.to, 'yyyyMMdd'),
+                            },
+                            dateRange: {
+                                from: targetRange.from, to: targetRange.to
+                            },
+                            inputFocus: null,
+                        }))
+                    return;
+                }
             }
-            setState((prev) => (
-                {
-                    ...prev,
-                    dateRangeString: {
-                        ...prev.dateRangeString,
-                        to: isAfter(selectedDay,getMaxEndDay(state.dateRange.from!)) ? format(getMaxEndDay(prev.dateRange.from!),'yyyyMMdd') : format(selectedDay,'yyyyMMdd')
-                    },
-                    dateRange: {
-                        ...prev.dateRange,
-                        to: isAfter(selectedDay,getMaxEndDay(state.dateRange.from!)) ? getMaxEndDay(prev.dateRange.from!) : selectedDay
-                    },
-                    showCalendar: false,
-                    inputFocus: null,
-                }))
-            return;
         }
     }
 
     const handleChangeInputtedRangeDate = (e: React.ChangeEvent<HTMLInputElement>) => {
         e.preventDefault();
+        const id = e.target.id;
         if(Date.parse(e.target.value)) {
-            const targetDate = new Date(e.target.value);
-            if(isOverMaxEndDay(targetDate)) {
-                const id = e.target.id;
-                if (id === "from") {
-                    setState(
-                        (prev) => (
-                            {
-                                ...prev,
-                                dateRange: {from: maxEndDay, end: maxEndDay},
-                                dateRangeString: {from: format(maxEndDay, 'yyyyMMdd'), to: format(maxEndDay, 'yyyyMMdd'),},
-                                inputFocus: "from",
-                            }
-                        )
-                    )
-                    return;
-                } else {
-                    setState(
-                        (prev) => (
-                            {
-                                ...prev,
-                                dateRange: {...prev.dateRange, end: isAfter(targetDate, getMaxEndDay(state.dateRange.from ?? new Date())) ? getMaxEndDay(state.dateRange.from ?? new Date()): targetDate},
-                                dateRangeString: {...prev.dateRangeString, to: isAfter(targetDate, getMaxEndDay(prev.dateRange.from ?? new Date())) ? format(getMaxEndDay(prev.dateRange.from ?? new Date()),'yyyyMMdd') : format(getMaxEndDay(targetDate),'yyyyMMdd')},
-                                inputFocus: "to",
-                            }
-                        )
-                    )
-                    return;
-                }
+            const targetRange = adjustFromToDate(
+                new Date(e.target.value),
+                {from: state.dateRange.from!, to: state.dateRange.to!},
+                id === "from" ? "FROM" : "TO"
+            );
+            if(targetRange) {
+                setState(
+                    (prev) => (
+                        {
+                            ...prev,
+                            dateRange: {...targetRange},
+                            dateRangeString: {
+                                ...prev.dateRangeString,
+                                fromError: id === "from" ? false : prev.dateRangeString.fromError,
+                                toError: id === "to" ? false : prev.dateRangeString.toError,
+                                from: format(targetRange.from, 'yyyyMMdd'),
+                                to: format(targetRange.to, 'yyyyMMdd')
+                            },
+                        })
+                )
+            } else {
+                setState(
+                    (prev) => (
+                        {
+                            ...prev,
+                            dateRangeString: {
+                                ...prev.dateRangeString,
+                                [e.target.id]: e.target.value,
+                                fromError: id === "from",
+                                toError: id === "to",
+                            },
+                            inputFocus: e.target.id
+                        })
+                )
             }
+        } else {
+            setState(
+                (prev) => (
+                    {
+                        ...prev,
+                        dateRangeString: {
+                            ...prev.dateRangeString,
+                            [e.target.id]: e.target.value,
+                            fromError: id === "from",
+                            toError: id === "to",
+                        },
+                        inputFocus: e.target.id
+                    })
+            )
         }
-        setState(
-            (prev) => (
-                {
-                    ...prev,
-                    dateRange:
-                        Date.parse(e.target.value) ?
-                            {
-                                ...prev.dateRange, [e.target.id]: new Date(e.target.value)
-                            } : {...prev.dateRange},
-                    dateRangeString: {...prev.dateRangeString, [e.target.id]: e.target.value},
-                    inputFocus: e.target.id
-                })
-        )
     }
 
     return (
@@ -200,7 +279,7 @@ function CustomDateInput() {
                     >
                         <InputBase
                             id="from"
-                            sx={{ ...state.inputFocus === "from" && {borderBottom: "2px solid green"},ml: 1, flex: 1, minWidth: "140px" }}
+                            sx={{ ...state.inputFocus === "from" && {borderBottom: `2px solid ${state.dateRangeString.fromError ? 'red' : 'green'}`},ml: 1, flex: 1, minWidth: "140px" }}
                             placeholder="FromDate"
                             inputProps={{ 'aria-label': 'from date' }}
                             onClick={handleOnClickedInputBase}
@@ -218,7 +297,7 @@ function CustomDateInput() {
                     >
                         <InputBase
                             id="to"
-                            sx={{ ...state.inputFocus === "to" && {borderBottom: "2px solid green"},ml: 1, flex: 1, minWidth: "140px" }}
+                            sx={{ ...state.inputFocus === "to" && {borderBottom: `2px solid ${state.dateRangeString.toError ? 'red' : 'green'}`}, ml: 1, flex: 1, minWidth: "140px" }}
                             placeholder="ToDate"
                             inputProps={{ 'aria-label': 'to date' }}
                             onClick={handleOnClickedInputBase}
@@ -232,10 +311,14 @@ function CustomDateInput() {
                     component="div"
                     sx={{ marginTop: '5px', p: '10px',width: "630px" }}
                 >
+                    <style>{css}</style>
                     <DayPicker
-                        styles={{
-                            caption: { fontSize: '0.8rem' },
-                            head: { fontSize: '1rem' },
+                        modifiersClassNames={{
+                            selected: 'custom-selected',
+                            today: 'today'
+                        }}
+                        modifiersStyles={{
+                            disabled: { fontSize: '90%' }
                         }}
                         mode="range"
                         defaultMonth={state.dateRange.from}
